e9ca18ed28f88cf691a63255ca5bce1646b9b150
Phil <philipmathew101@gmail.com>
2021-09-12 20:28:34 -0400

    Initial Commit

---
+++ gpt3-request.py
@@ -0,0 +1,22 @@
+import openai
+
+from config import *
+
+
+openai.api_key = OPENAI_API_KEY
+
+with open(GPT_REQUEST_FILE, 'r') as f:
+    gpt_request = f.read()
+
+response = openai.Completion.create(
+  engine="davinci",
+  prompt=gpt_request,
+  temperature=0.8,
+  max_tokens=896,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0,
+  stop=["====="]
+)
+
+print(response)
